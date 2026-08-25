288a3394f3cfe0592ffabfd9fb15c0d9f63b520e
sasami2577 <mr.pain20000525@gmail.com>
2026-08-26 00:35:10 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+/*
+  Supabase設定ファイル
+
+  ここにSupabaseの
+  ・Project URL
+  ・anon / publishable key
+  を入力します。
+
+  ※ service_role key は絶対に入れないでください。
+*/
+
+window.ONSEN_SUPABASE_CONFIG = {
+  url: "",
+  anonKey: ""
+};
